cca4f3e68ee10deb6df9023bf56c555bca72d0f6
Ian D <dunni@gnu.org>
2013-07-31 09:53:28 -0400

    interop-isar.h (global): Added notes on Isar interoperability.

--- src/interop-isar.h
+++ src/interop-isar.h
@@ -43,6 +43,8 @@ enum KEY_FUNCS {
   KF_NUM_FUNCS,
 };
 
+int isar_run_cmds (unsigned char ** inputs, unsigned char ** output);
+
 unsigned char * correct_conditionals (unsigned char * in_str);
 unsigned char * isar_to_aris (char * isar);
 int parse_thy (char * filename, proof_t * proof);
@@ -58,6 +60,26 @@ int isar_parse_primrec (char * primrec, char ** out_str);
 int isar_parse_def (char * def, char ** out_str);
 int isar_parse_datatype (char * dt, vec_t * constructors);
 
+/*
+const char * marksups_target[] = {
+  "chapter",
+  "section",
+  "subsection",
+  "subsubsection",
+  "text"
+};
+
+const char * marksups[] = {
+  "header",
+  "text_raw",
+  "sect",
+  "subsect",
+  "subsubsect",
+  "txt",
+  "txt_raw"
+};
+*/
+
 static key_func kfs[KF_NUM_FUNCS] = {
   "type_synonym", isar_parse_syn,
   "fun", isar_parse_fun,
@@ -66,6 +88,31 @@ static key_func kfs[KF_NUM_FUNCS] = {
   "definition", isar_parse_def,
   "lemma", isar_parse_lemma,
   "theorem", isar_parse_theorem,
+  /*
+  "header", isar_parse_markup,
+  "section", isar_parse_markup,
+  "subsection", isar_parse_markup,
+  "subsubsection", isar_parse_markup
+  */
 };
+
+struct regexp_assoc {
+  char * ident;
+  char * regexp;
+};
+
+/*
+struct regexp_assoc reg_assocs[] = {
+  "greek", "(\\<alpha> | \\<beta> | \\<gamma> | \\<delta> | \\<epsilon>\
+ | \\<zeta> | \\<eta> | \\<theta> | \\<iota> | \\<kappa> | \\<mu>      \
+ | \\<nu> | \\<xi> | \\<pi> | \\<rho> | \\<sigma> | \\<tau>	       \
+ | \\<upsilon> | \\<phi> | \\<chi> | \\<psi> | \\<omega> | \\<Gamma>   \
+ | \\<Delta> | \\<Theta> | \\<Lambda> | \\<Xi> | \\<Pi> | \\<Sigma>    \
+ | \\<Upsilon> | \\<Phi> | \\<Psi> | \\<Omega>)"
+  "sym", "[!#$%&*+-/<=>?@^_|~]"
+  "digit", "[0-9]",
+  "latin", "[a-zA-Z]"
+};
+*/
 
 #endif  /*  ARIS_INTEROP_ISAR_H  */
