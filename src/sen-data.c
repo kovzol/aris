21bbd11cae5ba03a90a572107fb149db95e68f01
Alvi16 <alvira.de-souza@epitech.eu>
2026-03-27 19:49:10 +0100

    fix: Update CmakeList from old to new and fix seen.data.c

--- src/sen-data.c
+++ src/sen-data.c
@@ -334,8 +334,8 @@ sen_data_evaluate (sen_data * sd, int * ret_val, list_t * pf_vars, list_t * line
         sen_data * ref_data;
 
         if (sd->refs[i] > lines->num_stuff) {
-            dest/src/config.h.inroy_str_vec(refs);
-            *ref_val = VALUE_TYPE_REF;
+            destroy_str_vec(refs);
+            *ret_val = VALUE_TYPE_REF;
             return _("One of the sentence's references points to a non-existing line.");
         }
         cur_ref = ls_nth (lines, sd->refs[i] - 1);
