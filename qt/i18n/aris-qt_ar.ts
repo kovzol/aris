ba74658cb5dce8beac3d3fbb2afe3275af17b60f
Khush Patel <khush2236@gmail.com>
2026-06-23 23:33:34 +0530

    i8n support for arab

--- qt/i18n/aris-qt_ar.ts
+++ qt/i18n/aris-qt_ar.ts
@@ -1,126 +1,176 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="ar_EG" sourcelanguage="en_US">
+<TS version="2.1" language="ar_SA" sourcelanguage="en_US">
+<context>
+    <name>KeyGroup</name>
+    <message>
+        <source>Conjunction</source>
+        <translation>اقتران</translation>
+    </message>
+    <message>
+        <source>Disjunction</source>
+        <translation>انفصال</translation>
+    </message>
+    <message>
+        <source>Negation</source>
+        <translation>نفي</translation>
+    </message>
+    <message>
+        <source>Implication</source>
+        <translation>استلزام</translation>
+    </message>
+    <message>
+        <source>Bi-conditional</source>
+        <translation>ثنائي الشرط</translation>
+    </message>
+    <message>
+        <source>For all</source>
+        <translation>لكل</translation>
+    </message>
+    <message>
+        <source>There exists</source>
+        <translation>يوجد</translation>
+    </message>
+    <message>
+        <source>Tautology</source>
+        <translation>تحصيل حاصل</translation>
+    </message>
+    <message>
+        <source>Contradiction</source>
+        <translation>تناقض</translation>
+    </message>
+    <message>
+        <source>Belongs To</source>
+        <translation>ينتمي إلى</translation>
+    </message>
+    <message>
+        <source>Null</source>
+        <translation>خالي</translation>
+    </message>
+    <message>
+        <source>XOR</source>
+        <translation>فصل حصري</translation>
+    </message>
+</context>
 <context>
     <name>DrawerTools</name>
+    <message><source>New</source><translation>جديد</translation></message>
+    <message><source>Reset</source><translation>إعادة ضبط</translation></message>
+    <message><source>Open</source><translation>فتح</translation></message>
+    <message><source>Save</source><translation>حفظ</translation></message>
+    <message><source>Save As</source><translation>حفظ باسم</translation></message>
+    <message><source>Export To LaTeX</source><translation>تصدير إلى LaTeX</translation></message>
+    <message><source>Toggle Goals</source><translation>تبديل الأهداف</translation></message>
+    <message><source>Toggle Dark Mode</source><translation>تبديل الوضع الداكن</translation></message>
+    <message><source>Zoom</source><translation>تكبير</translation></message>
+    <message><source>Reset Zoom</source><translation>إعادة ضبط التكبير</translation></message>
+    <message><source>Paste Proof from Clipboard</source><translation>لصق الإثبات من الحافظة</translation></message>
     <message>
-        <location filename="../DrawerTools.qml" line="36"/>
-        <source>New</source>
-        <translation>جديد</translation>
+        <source>(Ctrl+Shift+V)</source>
+        <translation>(Ctrl+Shift+V)</translation>
     </message>
+    <message><source>Copy Selected Lines</source><translation>نسخ الأسطر المحددة</translation></message>
     <message>
-        <location filename="../DrawerTools.qml" line="49"/>
-        <source>Reset</source>
-        <translation>إعادة التعيين</translation>
+        <source>(Ctrl+Shift+C)</source>
+        <translation>(Ctrl+Shift+C)</translation>
     </message>
-    <message>
-        <location filename="../DrawerTools.qml" line="57"/>
-        <source>Open</source>
-        <translation>فتح</translation>
-    </message>
-    <message>
-        <location filename="../DrawerTools.qml" line="68"/>
-        <source>Save</source>
-        <translation>حفظ</translation>
-    </message>
-    <message>
-        <location filename="../DrawerTools.qml" line="85"/>
-        <source>Save As</source>
-        <translation>حفظ بإسم</translation>
-    </message>
-    <message>
-        <location filename="../DrawerTools.qml" line="101"/>
-        <source>Export To LaTeX</source>
-        <translation>تصدير إلى لاتيك</translation>
-    </message>
-    <message>
-        <location filename="../DrawerTools.qml" line="120"/>
-        <source>Toggle Goals</source>
-        <translation>تبديل الأهداف</translation>
-    </message>
-    <message>
-        <location filename="../DrawerTools.qml" line="131"/>
-        <source>Toggle Dark Mode</source>
-        <translation>تبديل الوضع الداكن</translation>
-    </message>
-    <message>
-        <location filename="../DrawerTools.qml" line="139"/>
-        <source>Import Proof</source>
-        <translation>استرد برهان</translation>
-    </message>
-    <message>
-        <location filename="../DrawerTools.qml" line="163"/>
-        <source>Font</source>
-        <translation>الخط</translation>
-    </message>
-    <message>
-        <location filename="../DrawerTools.qml" line="173"/>
-        <source>Language</source>
-        <translation>اللغة</translation>
-    </message>
-    <message>
-        <location filename="../DrawerTools.qml" line="199"/>
-        <source>Help</source>
-        <translation>مساعدة</translation>
-    </message>
-    <message>
-        <location filename="../DrawerTools.qml" line="207"/>
-        <source>About</source>
-        <translation>حول</translation>
-    </message>
+    <message><source>Import Proof</source><translation>استيراد إثبات</translation></message>
+    <message><source>Font</source><translation>خط</translation></message>
+    <message><source>Language</source><translation>لغة</translation></message>
+    <message><source>Help</source><translation>مساعدة</translation></message>
+    <message><source>About</source><translation>حول</translation></message>
 </context>
 <context>
     <name>main</name>
     <message>
-        <location filename="../main.qml" line="77"/>
         <source>QT_LAYOUT_DIRECTION</source>
         <comment>QGuiApplication</comment>
         <translation>RTL</translation>
     </message>
+    <message><source>Unsaved changes</source><translation>تغييرات غير محفوظة</translation></message>
+    <message><source>Save file before closing?</source><translation>هل تريد حفظ الملف قبل الإغلاق؟</translation></message>
+    <message><source>Save new file before closing?</source><translation>هل تريد حفظ الملف الجديد قبل الإغلاق؟</translation></message>
+    <message><source>Reset the current proof and goals?</source><translation>إعادة ضبط الإثبات والأهداف الحالية؟</translation></message>
+    <message><source>Cancel</source><translation>إلغاء</translation></message>
+    <message><source>Reset</source><translation>إعادة ضبط</translation></message>
+    <message><source>Choose import behavior</source><translation>اختر سلوك الاستيراد</translation></message>
+    <message><source>Overwrite</source><translation>استبدال</translation></message>
+    <message><source>Append End</source><translation>إلحاق في النهاية</translation></message>
+    <message><source>Prepend</source><translation>إضافة في البداية</translation></message>
+</context>
+<context>
+    <name>ProofArea</name>
+    <message><source>Confirm Conversion</source><translation>تأكيد التحويل</translation></message>
+    <message><source>Cancel</source><translation>إلغاء</translation></message>
+    <message><source>Convert Anyway</source><translation>التحويل على أي حال</translation></message>
+    <message><source>Convert</source><translation>تحويل</translation></message>
+    <message><source>Convert to Conclusion</source><translation>تحويل إلى استنتاج</translation></message>
+    <message><source>Convert to Premise</source><translation>تحويل إلى فرضية</translation></message>
+
+    <message><source>Add Premise Above</source><translation>إضافة فرضية أعلى</translation></message>
+    <message><source>Add Conclusion Below</source><translation>إضافة استنتاج أسفل</translation></message>
+    <message><source>Add Premise</source><translation>إضافة فرضية</translation></message>
+    <message><source>Add Conclusion</source><translation>إضافة استنتاج</translation></message>
+    <message><source>Start Subproof</source><translation>بدء إثبات فرعي</translation></message>
+    <message><source>End Subproof</source><translation>إنهاء الإثبات الفرعي</translation></message>
+    <message><source>Remove this Line</source><translation>إزالة هذا السطر</translation></message>
+
+    <message><source>premise</source><translation>فرضية</translation></message>
+    <message><source>subproof</source><translation>إثبات فرعي</translation></message>
     <message>
-        <location filename="../main.qml" line="359"/>
-        <source>Reset Proof</source>
-        <translation>إعادة تعيين البرهان</translation>
+        <source>sf</source>
+        <translation>إ.ف</translation>
     </message>
+
+    <message><source>Inference</source><translation>استدلال</translation></message>
+    <message><source>Equivalence</source><translation>تكافؤ</translation></message>
+    <message><source>Predicate</source><translation>محمول</translation></message>
+    <message><source>Miscellaneous</source><translation>متنوع</translation></message>
+    <message><source>Boolean</source><translation>منطقي</translation></message>
+
+    <message><source>Modus Ponens</source><translation>قياس استثنائي</translation></message>
+    <message><source>Addition</source><translation>إضافة</translation></message>
+    <message><source>Simplification</source><translation>تبسيط</translation></message>
+    <message><source>Conjunction</source><translation>اقتران</translation></message>
+    <message><source>Hypothetical Syllogism</source><translation>قياس شرطي</translation></message>
+    <message><source>Disjunctive Syllogism</source><translation>قياس منفصل</translation></message>
+    <message><source>Excluded middle</source><translation>الثالث المرفوع</translation></message>
+    <message><source>Constructive Dilemma</source><translation>معضلة بناءة</translation></message>
+    <message><source>XOR Introduction</source><translation>إدخال الفصل الحصري</translation></message>
+    <message><source>XOR Elimination</source><translation>إزالة الفصل الحصري</translation></message>
+
+    <message><source>Implication</source><translation>استلزام</translation></message>
+    <message><source>DeMorgan</source><translation>دي مورغان</translation></message>
+    <message><source>Association</source><translation>تجميع</translation></message>
+    <message><source>Commutativity</source><translation>تبديل</translation></message>
+    <message><source>Idempotence</source><translation>تساوي القوى</translation></message>
+    <message><source>Distribution</source><translation>توزيع</translation></message>
+    <message><source>Double Negation</source><translation>نفي مزدوج</translation></message>
+    <message><source>Exportation</source><translation>تصدير</translation></message>
+    <message><source>Subsumption</source><translation>إدراج</translation></message>
+    <message><source>Contrapositive</source><translation>عكس نقيض</translation></message>
+
+    <message><source>Universal Generalization</source><translation>تعميم كلي</translation></message>
+    <message><source>Universal Instantiation</source><translation>تخصيص كلي</translation></message>
+    <message><source>Existential Generalization</source><translation>تعميم وجودي</translation></message>
+    <message><source>Existential Instantiation</source><translation>تخصيص وجودي</translation></message>
+    <message><source>Bound Variable Substitution</source><translation>استبدال متغير مقيد</translation></message>
+    <message><source>Null Quantifier</source><translation>مكمم صفري</translation></message>
+    <message><source>Prenex</source><translation>صيغة برينكس</translation></message>
+    <message><source>Identity</source><translation>تطابق</translation></message>
+    <message><source>Free Variable Substitution</source><translation>استبدال متغير حر</translation></message>
+
+    <message><source>Lemma</source><translation>تمهيدية</translation></message>
+    <message><source>Subproof</source><translation>إثبات فرعي</translation></message>
+    <message><source>Sequence</source><translation>تسلسل</translation></message>
+    <message><source>Induction</source><translation>استقراء</translation></message>
+
     <message>
-        <location filename="../main.qml" line="423"/>
-        <source>Import Proof</source>
-        <translation>استيراد البرهان</translation>
+        <source>Identity </source>
+        <translation>تطابق </translation>
     </message>
-    <message>
-        <location filename="../main.qml" line="434"/>
-        <source>Choose import behavior</source>
-        <translation>اختر طريقة الاستيراد</translation>
-    </message>
-    <message>
-        <location filename="../main.qml" line="446"/>
-        <source>Overwrite</source>
-        <translation>استبدال</translation>
-    </message>
-    <message>
-        <location filename="../main.qml" line="459"/>
-        <source>Append End</source>
-        <translation>إلحاق في النهاية</translation>
-    </message>
-    <message>
-        <location filename="../main.qml" line="472"/>
-        <source>Prepend</source>
-        <translation>إضافة في البداية</translation>
-    </message>
-    <message>
-        <location filename="../main.qml" line="370"/>
-        <source>Reset the current proof and goals?</source>
-        <translation>هل تريد إعادة تعيين البرهان والأهداف الحالية؟</translation>
-    </message>
-    <message>
-        <location filename="../main.qml" line="380"/>
-        <source>Cancel</source>
-        <translation>إلغاء</translation>
-    </message>
-    <message>
-        <location filename="../main.qml" line="391"/>
-        <source>Reset</source>
-        <translation>إعادة التعيين</translation>
-    </message>
+    <message><source>Negation</source><translation>نفي</translation></message>
+    <message><source>Dominance</source><translation>هيمنة</translation></message>
+    <message><source>Symbol Negation</source><translation>نفي الرمز</translation></message>
 </context>
 </TS>
